8b7196cc00d4672062b81fbccb653196dd58bf38
averyjohnston <averyejohnston@gmail.com>
2024-11-14 12:40:27 -0600

    feat: add simple starting form

--- src/side-panel.js
+++ src/side-panel.js
@@ -1,13 +1,17 @@
-async function handleClick() {
+async function handleSubmit(e) {
+  e.preventDefault();
+  const data = new FormData(e.target);
+  const entries = Object.fromEntries(data.entries());
+
   const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
   chrome.scripting.insertCSS({
     css: `
 .mediumEditorSpace p {
-  font-family: 'Comic Sans MS' !important;
+  font-family: ${entries.fontFamily} !important;
 }
 `,
     target: { tabId: tab.id },
   });
 }
 
-document.querySelector('button').addEventListener('click', handleClick);
+document.querySelector('form').addEventListener('submit', handleSubmit);
